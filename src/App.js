8ae8f684f72975d8d969df03956e6b902875d312
sutandimalaw <sutandimalaw@gmail.com>
2022-03-08 19:38:34 +0700

    client

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import { Home } from './Pages/Home';
+import Login from './Pages/Login';
+import Product from './Pages/Product';
+import ProductList from './Pages/ProductList';
+import Register from './Pages/Register';
+import Cart from './Pages/Cart';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+import Success from './Pages/Success';
+import { useSelector } from 'react-redux';
+
+const App = () => {
+  const user = useSelector((state) => state.user.currentUser);   
+  return(
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route path="/products/:category">
+          <ProductList/>
+        </Route>
+        <Route path="/product/:id">
+          <Product/>
+        </Route>
+        <Route path="/cart">
+          <Cart/>
+        </Route>
+        <Route path="/success">
+          <Success/>
+        </Route>
+        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+        <Route path="/register">
+          {user ? <Redirect to="/" /> : <Register />}
+        </Route>
+      </Switch>
+    </Router>
+  )
+};
+
+export default App;
